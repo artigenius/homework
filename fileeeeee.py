b61f6207a938df62a831e6f898db3ec966eaecbd
artigenius <107915746+artigenius@users.noreply.github.com>
2023-09-28 21:24:00 +0700

    Delete fileeeeee.py

--- fileeeeee.py
+++
@@ -1,7 +0,0 @@
-# program that asks for the name
-print ( '- Как Вас зовут? ')
-name = input ()
-print ( '- На какой специальности вы обучаетесь? ')
-study = input ()
-print ( 'Я,', name, 'обучаюсь на направлении', study, 'и я хочу умерееееееть')
-input('Press ENTER to exit')
